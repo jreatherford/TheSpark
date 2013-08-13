899120d1380586507a003d0186ef4cdac2ae56bd
jreatherford <jamesreatherford@gmail.com>
2013-08-13 09:18:21 -0500

    Fixed Header

--- Footer.php
+++ Footer.php
@@ -5,7 +5,7 @@
     "Magic: The Gathering" font created by <a href ="http://www.urbanfonts.com/fonts.php?fontauthor=1114">Stickfronts</a> <br>
     This is a fan made site. I claim no rights or ownership to Magic The Gathering. <br>
     All rights to Magic the Gathering ownership belongs to 
-    <a href ="http://company.wizards.com/">Wizzards of the Coast</a>.<br>
-    Card data collected from <a href="http://gatherer.wizards.com/Pages/Default.aspx">The Gather</a>.<br>
+    <a href ="http://company.wizards.com/">Wizards of the Coast</a>.<br>
+    Card data collected from <a href="http://gatherer.wizards.com/Pages/Default.aspx">The Gatherer</a>.<br>
     <a href="https://github.com/jreatherford/TheSpark">View on Github</a>
 </footer>
